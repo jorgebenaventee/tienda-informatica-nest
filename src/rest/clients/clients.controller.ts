63ae487e30b96557fd0f0fa879beae3dcd4fe8b3
Jorge Benavente <jorge.benavente2003@gmail.com>
2024-02-08 16:17:30 +0100

    Add javadoc and swagger

--- src/rest/clients/clients.controller.ts
+++ src/rest/clients/clients.controller.ts
@@ -14,10 +14,13 @@ import {
 import { ClientsService } from './clients.service'
 import { CreateClientDto } from './dto/create-client.dto'
 import { UpdateClientDto } from './dto/update-client.dto'
-import { Paginate, PaginateQuery } from 'nestjs-paginate'
+import { Paginate, PaginateQuery, Paginated } from 'nestjs-paginate'
 import { JwtAuthGuard } from '../auth/jwt-auth/jwt-auth.guard'
 import { Roles, RolesGuard } from '../auth/roles/roles.guard'
+import { ApiBadRequestResponse, ApiBody, ApiNotFoundResponse, ApiParam, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { ClientResponseDto } from './dto/client-response.dto'
 
+@ApiTags('clients')
 @Controller('clients')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class ClientsController {
@@ -25,41 +28,105 @@ export class ClientsController {
 
   @Post()
   @Roles('employee')
+  @ApiBody({ type: CreateClientDto, description: 'Client to create' }) 
+  @ApiResponse({
+    status: 201,
+    description: 'Client created successfully',
+    type: ClientResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request',
+  })
   create(@Body() createClientDto: CreateClientDto) {
     if (Object.keys(createClientDto).length === 0) {
-      throw new BadRequestException('Cannot create an empty client')
+      throw new BadRequestException('Cannot create an empty client');
     }
-    return this.clientsService.create(createClientDto)
+    return this.clientsService.create(createClientDto);
   }
 
   @Get()
   @Roles('client')
+  @ApiResponse({
+    status: 200,
+    description: 'List of clients paginated',
+    type: Paginated<ClientResponseDto>,
+  })
+  @ApiQuery({
+    description: 'Filter by limit',
+    name: 'limit',
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    description: 'Filter by page',
+    name: 'page',
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    description: 'Filter by sort, ASC or DESC',
+    name: 'sortBy',
+    required: false,
+    type: String,
+  })
   async findAll(@Paginate() query: PaginateQuery) {
-    return await this.clientsService.findAll(query)
+    return await this.clientsService.findAll(query);
   }
 
   @Get(':id')
   @Roles('client')
+  @ApiResponse({
+    status: 200,
+    description: 'Return a client by id',
+    type: ClientResponseDto,
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Client id',
+    type: Number,
+  })
+  @ApiNotFoundResponse({
+    description: 'Client not found',
+  })
+  @ApiBadRequestResponse({
+    description: 'Invalid client id',
+  })
   async findOne(@Param('id', ParseIntPipe) id: string) {
-    return await this.clientsService.findOne(+id)
+    return await this.clientsService.findOne(+id);
   }
 
   @Patch(':id')
   @Roles('employee')
+  @ApiBody({ type: UpdateClientDto, description: 'Client to update' }) 
+  @ApiResponse({
+    status: 200,
+    description: 'Client updated successfully',
+    type: ClientResponseDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'Client not found',
+  })
   async update(
     @Param('id', ParseIntPipe) id: string,
     @Body() updateClientDto: UpdateClientDto,
   ) {
     if (Object.keys(updateClientDto).length === 0) {
-      throw new BadRequestException('Cannot update an empty client')
+      throw new BadRequestException('Cannot update an empty client');
     }
-    return await this.clientsService.update(+id, updateClientDto)
+    return await this.clientsService.update(+id, updateClientDto);
   }
 
   @Delete(':id')
   @HttpCode(204)
   @Roles('employee')
+  @ApiResponse({
+    status: 204,
+    description: 'Client deleted successfully',
+  })
+  @ApiNotFoundResponse({
+    description: 'Client not found',
+  })
   async remove(@Param('id') id: string) {
-    return await this.clientsService.remove(+id)
+    return await this.clientsService.remove(+id);
   }
 }
--- src/rest/clients/clients.service.ts
+++ src/rest/clients/clients.service.ts
@@ -1,8 +1,8 @@
 import {
-  BadRequestException,
-  Inject,
-  Injectable,
-  NotFoundException,
+	BadRequestException,
+	Inject,
+	Injectable,
+	NotFoundException,
 } from '@nestjs/common'
 import { CreateClientDto } from './dto/create-client.dto'
 import { UpdateClientDto } from './dto/update-client.dto'
@@ -14,14 +14,24 @@ import { Employee } from '../employees/entities/employee.entity'
 import { CACHE_MANAGER } from '@nestjs/cache-manager'
 import { Cache } from 'cache-manager'
 import {
-  FilterOperator,
-  paginate,
-  Paginated,
-  PaginateQuery,
+	FilterOperator,
+	paginate,
+	Paginated,
+	PaginateQuery,
 } from 'nestjs-paginate'
 import { hash } from 'typeorm/util/StringUtils'
 import { ClientResponseDto } from './dto/client-response.dto'
 
+/**
+ * Servicio inyectable para gestionar clientes.
+ *
+ * @class
+ * @constructor
+ * @param {Repository<Client>} clientRepository - Repositorio para entidades Client.
+ * @param {Repository<Employee>} employeeRepository - Repositorio para entidades Employee.
+ * @param {ClientMapper} clientMapper - Mapper para la transformación entre DTO y entidades.
+ * @param {Cache} cacheManager - Gestor de caché para almacenar datos en caché.
+ */
 @Injectable()
 export class ClientsService {
   constructor(
@@ -32,6 +42,15 @@ export class ClientsService {
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
+  /**
+   * Crea un nuevo cliente.
+   *
+   * @async
+   * @function
+   * @param {CreateClientDto} createClientDto - Datos para crear un nuevo cliente.
+   * @throws {BadRequestException} Lanza una excepción si el correo electrónico ya existe...
+   * @returns {Promise<ClientResponseDto>} El cliente creado.
+   */
   async create(createClientDto: CreateClientDto) {
     const existsEmployee = await this.employeeRepository.exist({
       where: { email: createClientDto.email },
@@ -49,6 +68,14 @@ export class ClientsService {
     return this.clientMapper.fromEntity(savedClient)
   }
 
+  /**
+   * Encuentra todos los clientes basados en parámetros de paginación...
+   *
+   * @async
+   * @function
+   * @param {PaginateQuery} query - Parámetros de consulta de paginación...
+   * @returns {Promise<Paginated<ClientResponseDto>>} Lista paginada de clientes.
+   */
   async findAll(query: PaginateQuery): Promise<Paginated<ClientResponseDto>> {
     const cache = await this.cacheManager.get<Paginated<ClientResponseDto>>(
       `all_clients_${hash(JSON.stringify(query))}`,
@@ -78,6 +105,15 @@ export class ClientsService {
     return response as Paginated<ClientResponseDto>
   }
 
+  /**
+   * Encuentra un cliente por ID.
+   *
+   * @async
+   * @function
+   * @param {number} id - El ID del cliente a buscar.
+   * @throws {NotFoundException} Lanza una excepción si el cliente no se encuentra.
+   * @returns {Promise<ClientResponseDto>} El cliente encontrado.
+   */
   async findOne(id: number) {
     const cache = await this.cacheManager.get(`client_${id}`)
     if (cache) {
@@ -92,6 +128,17 @@ export class ClientsService {
     return response
   }
 
+  /**
+   * Actualiza un cliente por ID.
+   *
+   * @async
+   * @function
+   * @param {number} id - El ID del cliente a actualizar.
+   * @param {UpdateClientDto} updateClientDto - Datos para actualizar el cliente.
+   * @throws {BadRequestException} Lanza una excepción si el correo electrónico ya existe...
+   * @throws {NotFoundException} Lanza una excepción si el cliente no se encuentra.
+   * @returns {Promise<ClientResponseDto>} El cliente actualizado.
+   */
   async update(id: number, updateClientDto: UpdateClientDto) {
     const existsEmployee = await this.employeeRepository.exist({
       where: { email: updateClientDto.email },
@@ -117,6 +164,14 @@ export class ClientsService {
     return this.clientMapper.fromEntity(savedClient)
   }
 
+  /**
+   * Elimina un cliente por ID.
+   *
+   * @async
+   * @function
+   * @param {number} id - El ID del cliente a eliminar.
+   * @throws {NotFoundException} Lanza una excepción si el cliente no se encuentra.
+   */
   async remove(id: number) {
     const client = await this.clientRepository.findOne({ where: { id } })
     if (!client) {
@@ -125,14 +180,28 @@ export class ClientsService {
     await this.clientRepository.remove(client)
     await this.invalidateCache(`client_${id}`)
   }
-
+ /**
+   * Invalida la caché basada en el patrón de clave especificado.
+   *
+   * @async
+   * @function
+   * @param {string} keyPattern - El patrón para hacer coincidir las claves de la caché a eliminar.
+   * @returns {Promise<void>} Una promesa que indica la finalización de la invalidación de la caché.
+   */
   async invalidateCache(keyPattern: string): Promise<void> {
     const cacheKeys = await this.cacheManager.store.keys()
     const keysToDelete = cacheKeys.filter((key) => key.startsWith(keyPattern))
     const promises = keysToDelete.map((key) => this.cacheManager.del(key))
     await Promise.all(promises)
   }
-
+  /**
+   * Encuentra un cliente por correo electrónico.
+   *
+   * @async
+   * @function
+   * @param {string} email - El correo electrónico del cliente a buscar.
+   * @returns {Promise<Client | null>} El cliente encontrado o nulo si no se encuentra.
+   */
   async findByEmail(email: string) {
     const client = await this.clientRepository.findOne({ where: { email } })
     if (!client) {
